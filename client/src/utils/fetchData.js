a52c8c4450fa81192d7df3040b7993d8e7151e83
OscarJVD <oscarjaviervargas@hotmail.com>
2022-03-09 14:54:00 -0500

    front vs prod back

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+let BASE_URL = "https://pruebacolpatria.herokuapp.com"
 // const apiBaseUrl = process.env.REACT_APP_API_URL;
 
 export const getDataAPI = async (url, token) => {
-  const res = await axios.get(`/api/${url}`, {
+  const res = await axios.get(`${BASE_URL}/api/${url}`, {
     headers: {
       Authorization: token,
       // headers: { Authorization: `Bearer ${token}`
@@ -12,7 +13,7 @@ export const getDataAPI = async (url, token) => {
 };
 
 export const postDataAPI = async (url, post, token) => {
-  const res = await axios.post(`/api/${url}`, post, {
+  const res = await axios.post(`${BASE_URL}/api/${url}`, post, {
     headers: {
       Authorization: token,
       // headers: { Authorization: `Bearer ${token}`
@@ -22,7 +23,7 @@ export const postDataAPI = async (url, post, token) => {
 };
 
 export const putDataAPI = async (url, post, token) => {
-  const res = await axios.put(`/api/${url}`, post, {
+  const res = await axios.put(`${BASE_URL}/api/${url}`, post, {
     headers: {
       Authorization: token,
       // headers: { Authorization: `Bearer ${token}`
@@ -32,7 +33,7 @@ export const putDataAPI = async (url, post, token) => {
 };
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = await axios.patch(`/api/${url}`, post, {
+  const res = await axios.patch(`${BASE_URL}/api/${url}`, post, {
     headers: {
       Authorization: token,
       // headers: { Authorization: `Bearer ${token}`
@@ -42,7 +43,7 @@ export const patchDataAPI = async (url, post, token) => {
 };
 
 export const deleteDataAPI = async (url, token) => {
-  const res = await axios.delete(`/api/${url}`, {
+  const res = await axios.delete(`${BASE_URL}/api/${url}`, {
     headers: {
       Authorization: token,
       // headers: { Authorization: `Bearer ${token}`
